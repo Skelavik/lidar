83e5a9ba17d178da2e4a77e1cae92fb7c82cf165
Skelavik <Skelavik@yandex.ru>
2025-09-27 10:01:04 +0300

    delete Buffer

--- data_between_esp_rasp/app.py
+++ data_between_esp_rasp/app.py
@@ -4,7 +4,6 @@ import struct
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 8888
-BUFFER_SIZE = 1  # Должно совпадать с ESP32
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
@@ -12,11 +11,10 @@ sock.bind((UDP_IP, UDP_PORT))
 print("Жду данные от LiDAR...")
 
 while True:
-    data, addr = sock.recvfrom(BUFFER_SIZE * 2)  # uint16_t = 2 байта
-    if len(data) == BUFFER_SIZE * 2:
+    data, addr = sock.recvfrom(2)  # uint16_t = 2 байта
+    if len(data) == 2:
         # Распаковываем как 100 целых чисел (беззнаковых, 2 байта)
-        distances = struct.unpack('<' + 'H' * BUFFER_SIZE, data)  # '<' = little-endian
-        for d in distances:
-            print(d)  # Каждое значение в отдельной строке
+        dist = struct.unpack('<' + 'H', data)[0]  # '<' = little-endian
+        print(dist)  # Каждое значение в отдельной строке
     else:
         print(f"Неверный размер пакета: {len(data)} байт")
